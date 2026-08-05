e730f02c16c791bbe4914c1b9fa9e61e4380fdd9
Aravind Metku <ametku@newrelic.com>
2026-08-05 09:49:32 +0530

    Revert "feat: add PWA support with next-pwa service worker"
    This reverts commit 93a9f4e1948c13c91a980dcb118c1aae7b0e59b6.

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,5 @@
 import type { NextConfig } from 'next'
-import withPWA from '@ducanh2912/next-pwa'
 
-const nextConfig: NextConfig = {}
+const nextConfig: NextConfig = {/* config options here */}
 
-export default withPWA({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
-  cacheOnFrontEndNav: true,
-  aggressiveFrontEndNavCaching: true,
-  reloadOnOnline: true,
-})(nextConfig)
+export default nextConfig
