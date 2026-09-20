d449001b43b1bce86a244c9baaa30da772de9a10
Aravind Metku <ametku@newrelic.com>
2026-09-20 08:38:57 +0530

    feat: constrain map pan/zoom to park bounds (maxBounds+0.003°, minZoom 14)

--- components/map/ParkMapbox.tsx
+++ components/map/ParkMapbox.tsx
@@ -12,6 +12,12 @@ const ORTHO_ID = 'aravindmetku.nedour'
 const ORTHO_BOUNDS: [number, number, number, number] = [
   78.45778053580075, 17.934780490162368, 78.46515161551673, 17.940598787066882,
 ]
+const MAX_BOUNDS: [number, number, number, number] = [
+  ORTHO_BOUNDS[0] - 0.003,
+  ORTHO_BOUNDS[1] - 0.003,
+  ORTHO_BOUNDS[2] + 0.003,
+  ORTHO_BOUNDS[3] + 0.003,
+]
 
 export default function ParkMapbox({ onLoadError }: ParkMapboxProps) {
   const containerRef = useRef<HTMLDivElement>(null)
@@ -22,6 +28,8 @@ export default function ParkMapbox({ onLoadError }: ParkMapboxProps) {
     const map = new mapboxgl.Map({
       container: containerRef.current!,
       style: 'mapbox://styles/mapbox/satellite-v9',
+      maxBounds: MAX_BOUNDS,
+      minZoom: 14,
     })
 
     map.on('load', () => {
