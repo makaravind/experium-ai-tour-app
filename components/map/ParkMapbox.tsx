dfa8eece5c8ab376b5848707836c49f4773bc920
Aravind Metku <ametku@newrelic.com>
2026-09-20 08:14:49 +0530

    feat: instantiate Mapbox map with satellite basemap

--- components/map/ParkMapbox.tsx
+++ components/map/ParkMapbox.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useRef } from 'react'
+import { useEffect, useRef } from 'react'
 import mapboxgl from 'mapbox-gl'
 import 'mapbox-gl/dist/mapbox-gl.css'
 
@@ -10,6 +10,15 @@ interface ParkMapboxProps {
 
 export default function ParkMapbox({ onLoadError }: ParkMapboxProps) {
   const containerRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_TOKEN!
+
+    const map = new mapboxgl.Map({
+      container: containerRef.current!,
+      style: 'mapbox://styles/mapbox/satellite-v9',
+    })
+  }, [])
 
   return <div ref={containerRef} className="absolute inset-0" />
 }
